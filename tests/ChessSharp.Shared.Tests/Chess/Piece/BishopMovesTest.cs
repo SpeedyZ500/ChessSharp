21fd6d4a4eb1ba75babdf0a1ed43d7639e7144a6
SpeedyZ500 <92234505+SpeedyZ500@users.noreply.github.com>
2026-01-07 13:03:10 -0700

    Finish Bishop Tests

--- tests/ChessSharp.Shared.Tests/Chess/Piece/BishopMovesTest.cs
+++ tests/ChessSharp.Shared.Tests/Chess/Piece/BishopMovesTest.cs
@@ -29,4 +29,50 @@ public class BishopMovesTest
         // Then
         TestUtilities.ValidateMoves(board, position, endPositions);
     }
+
+    [Fact]
+    public void BishopCaptureEnemy()
+    {
+        // Given
+        string board = """
+        | | | | | | | | |
+        | | | |Q| | | | |
+        | | | | | | | | |
+        | |b| | | | | | |
+        |r| | | | | | | |
+        | | | | | | | | |
+        | | | | |P| | | |
+        | | | | | | | | |
+        """;
+        ChessPosition position = new ChessPosition(5, 2);
+        int[,] endPositions =
+        {
+            {6, 3}, {7, 4},
+            {4, 3}, {3, 4}, {2, 5},
+            // none
+            {6, 1},
+        };
+        // Then
+        TestUtilities.ValidateMoves(board, position, endPositions);
+    }
+
+    [Fact]
+    public void BishopBlocked()
+    {
+        // Given
+        string board = """
+        | | | | | | | | |
+        | | | | | | | | |
+        | | | | | | | | |
+        | | | | | | | | |
+        | | | | | | | | |
+        | | | | | | | | |
+        | | | | |R| |P| |
+        | | | | | |B| | |
+        """;
+        ChessPosition position = new ChessPosition(1, 6);
+        int[,] endPositions = {};
+        // Then
+        TestUtilities.ValidateMoves(board, position, endPositions);
+    }
 }
