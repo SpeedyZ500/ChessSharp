e6ff000b6000c12aa3e10cfc5b312d0077d33e5c
SpeedyZ500 <92234505+SpeedyZ500@users.noreply.github.com>
2026-01-07 13:53:59 -0700

    Finish up the Knight Movement tests

--- tests/ChessSharp.Shared.Tests/Chess/Piece/KnightMovesTests.cs
+++ tests/ChessSharp.Shared.Tests/Chess/Piece/KnightMovesTests.cs
@@ -93,6 +93,174 @@ public class KnightMovesTest
     }
 
     [Fact]
+    public void KnightEdgeOfBoardBottom()
+    {
+        // Given
+        string board = """
+        | | | | | | | | |
+        | | | | | | | | |
+        | | | | | | | | |
+        | | | | | | | | |
+        | | | | | | | | |
+        | | | | | | | | |
+        | | | | | | | | |
+        | | | | | |N| | |
+        """;
+        ChessPosition position = new ChessPosition(1, 6);
+        int[,] endPositions ={{2, 4}, {3, 5}, {3, 7}, {2, 8}};
+        // Then
+        TestUtilities.ValidateMoves(board, position, endPositions);
+    }
+
+    [Fact]
+    public void KnightEdgeOfBoardTop()
+    {
+        // Given
+        string board = """
+        | | |N| | | | | |
+        | | | | | | | | |
+        | | | | | | | | |
+        | | | | | | | | |
+        | | | | | | | | |
+        | | | | | | | | |
+        | | | | | | | | |
+        | | | | | | | | |
+        """;
+        ChessPosition position = new ChessPosition(8, 3);
+        int[,] endPositions ={{7, 5}, {6, 4}, {6, 2}, {7, 1}};
+        // Then
+        TestUtilities.ValidateMoves(board, position, endPositions);
+    }
+
+    [Fact]
+    public void KnightEdgeOfBoardBottomRight()
+    {
+        // Given
+        string board = """
+        | | | | | | | | |
+        | | | | | | | | |
+        | | | | | | | | |
+        | | | | | | | | |
+        | | | | | | | | |
+        | | | | | | | | |
+        | | | | | | | | |
+        | | | | | | | |N|
+        """;
+        ChessPosition position = new ChessPosition(1, 8);
+        int[,] endPositions ={{2, 6}, {3, 7}};
+        // Then
+        TestUtilities.ValidateMoves(board, position, endPositions);
+    }
+
+    [Fact]
+    public void KnightEdgeOfBoardTopRight()
+    {
+        // Given
+        string board = """
+        | | | | | | | |N|
+        | | | | | | | | |
+        | | | | | | | | |
+        | | | | | | | | |
+        | | | | | | | | |
+        | | | | | | | | |
+        | | | | | | | | |
+        | | | | | | | | |
+        """;
+        ChessPosition position = new ChessPosition(8, 8);
+        int[,] endPositions ={{6, 7}, {7, 6}};
+        // Then
+        TestUtilities.ValidateMoves(board, position, endPositions);
+    }
+
+    [Fact]
+    public void KnightEdgeOfBoardTopLeft()
+    {
+        // Given
+        string board = """
+        |N| | | | | | | |
+        | | | | | | | | |
+        | | | | | | | | |
+        | | | | | | | | |
+        | | | | | | | | |
+        | | | | | | | | |
+        | | | | | | | | |
+        | | | | | | | | |
+        """;
+        ChessPosition position = new ChessPosition(8, 1);
+        int[,] endPositions ={{7, 3}, {6, 2}};
+        // Then
+        TestUtilities.ValidateMoves(board, position, endPositions);
+    }
+
+    [Fact]
+    public void KnightEdgeOfBoardBottomLeft()
+    {
+        // Given
+        string board = """
+        | | | | | | | | |
+        | | | | | | | | |
+        | | | | | | | | |
+        | | | | | | | | |
+        | | | | | | | | |
+        | | | | | | | | |
+        | | | | | | | | |
+        |N| | | | | | | |
+        """;
+        ChessPosition position = new ChessPosition(1, 1);
+        int[,] endPositions ={{2, 3}, {3, 2}};
+        // Then
+        TestUtilities.ValidateMoves(board, position, endPositions);
+    }
+
+    [Fact]
+    public void KnightSurroundedButNotBlocked()
+    {
+        // Given
+        string board = """
+        | | | | | | | | |
+        | | | | | | | | |
+        | | | |R|R|R| | |
+        | | | |R|N|R| | |
+        | | | |R|R|R| | |
+        | | | | | | | | |
+        | | | | | | | | |
+        | | | | | | | | |
+        """;
+        ChessPosition position = new ChessPosition(5, 5);
+        int[,] endPositions =
+        {
+            {7, 6}, {6, 7}, {4, 7}, {3, 6}, {3, 4}, {4, 3}, {6, 3}, {7, 4},
+
+        };
+        // Then
+        TestUtilities.ValidateMoves(board, position, endPositions);
+    }
+
+    [Fact]
+    public void KnightBlocked()
+    {
+        // Given
+        string board = """
+        | | | | | | | | |
+        | | | |R| | | | |
+        | | | | | | |P| |
+        | | | | |N| | | |
+        | | |N| | | | | |
+        | | | | | | | | |
+        | | | | | | | | |
+        | | | | | | | | |
+        """;
+        ChessPosition position = new ChessPosition(5, 5);
+        int[,] endPositions =
+        {
+            {3, 4}, {3, 6}, {4, 7}, {7, 6}, {6, 3}
+
+        };
+        // Then
+        TestUtilities.ValidateMoves(board, position, endPositions);
+    }
+
+    [Fact]
     public void KnightCaptureEnemy()
     {
         // Given
